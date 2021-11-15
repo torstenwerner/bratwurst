1613ac4eca90c2286136299fd9d9d71149dfc2d4
Torsten Werner <torsten.werner@westernacher.com>
2021-11-15 21:14:47 +0100

    add age to ui

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,13 @@
 	import {fetchData, locations, ages} from './repository';
 	
 	let location = 'Bundesgebiet';
+	let age = ages[0];
 
-	let promise = fetchData(location, ages[0]);
+	let promise = fetchData(location, age);
 </script>
 
-<select bind:value={location} on:change="{() => promise = fetchData(location, ages[0])}">
+<label for="location">Land/Gebiet</label>
+<select name="location" bind:value={location} on:change="{() => promise = fetchData(location, age)}">
 	{#each locations as value}
 		<option value={value}>
 			{value}
@@ -15,7 +17,16 @@
 	{/each}
 </select>
 
+<label for="age">Altersgruppe</label>
+<select name="age" bind:value={age} on:change="{() => promise = fetchData(location, age)}">
+	{#each ages as value}
+		<option value={value}>
+			{value}
+		</option>
+	{/each}
+</select>
+
 {#await promise then data}
-	<Chart title="Inzidenz in {location}" data={data} type="line" height="800" lineOptions={{dotSize: 3, regionFill: 1}}
+	<Chart title="Inzidenz in {location} Altersgruppe {age}" data={data} type="line" height="800" lineOptions={{dotSize: 3, regionFill: 1}}
 		axisOptions={{xIsSeries: true, xAxisMode: 'tick'}} />
 {/await}
