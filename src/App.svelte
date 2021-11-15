d90a0248b63965cf62ccbc5e59b0d95f8da3be52
Torsten Werner <torsten.werner@westernacher.com>
2021-11-15 23:03:26 +0100

    parse rki data

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,13 @@
 <script>
 	import Chart from 'svelte-frappe-charts';
-	import {fetchData, locations, ages} from './repository';
+	import {promise, getData, locations, ages} from './repository';
 	
 	let location = 'Bundesgebiet';
 	let age = ages[0];
-
-	let promise = fetchData(location, age);
 </script>
 
 <label for="location">Land/Gebiet</label>
-<select name="location" bind:value={location} on:change="{() => promise = fetchData(location, age)}">
+<select name="location" bind:value={location}>
 	{#each locations as value}
 		<option value={value}>
 			{value}
@@ -18,7 +16,7 @@
 </select>
 
 <label for="age">Altersgruppe</label>
-<select name="age" bind:value={age} on:change="{() => promise = fetchData(location, age)}">
+<select name="age" bind:value={age}>
 	{#each ages as value}
 		<option value={value}>
 			{value}
@@ -27,6 +25,6 @@
 </select>
 
 {#await promise then data}
-	<Chart title="Hospitalisierungsinzidenz {location} Altersgruppe {age}" data={data} type="line" height="800" lineOptions={{dotSize: 3, regionFill: 1}}
+	<Chart title="Hospitalisierungsinzidenz {location} Altersgruppe {age}" data={getData(data, location, age)} type="line" height="800" lineOptions={{dotSize: 3, regionFill: 1}}
 		axisOptions={{xIsSeries: true, xAxisMode: 'tick'}} />
 {/await}
--- src/repository.js
+++ src/repository.js
@@ -1,11 +1,38 @@
-export function fetchData(location, age) {
-    console.log(age);
-    return fetch(`data/${location}_${age}.json`)
-        .then(response => response.json())
-        .then(json => ({
-            labels: json.map(row => row.date).reverse(),
-            datasets: [{values: json.map(row => row.incidence).reverse()}]
-        }));
+const url = 'https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv';
+
+function parse(csv) {
+    function reduceLocation(result, current) {
+        (result[current.location] = (result[current.location] || [])).push(current.value);
+        return result;
+    }
+
+    function reduceAge(result, current) {
+        (result[current.age] = (result[current.age] || [])).push(current.value);
+        return result;
+    }
+
+    const byLocation = csv.split('\n').slice(1)
+        .map(line => line.split(','))
+        .map(([date, location, , age, , incidence]) => ({location, value: {age, value: {date, incidence}}}))
+        .reduce((result, current) => reduceLocation(result, current), {});
+    const byLocationAndAge = Object.entries(byLocation)
+        .map(([location, value]) => ([location, value.reduce((result, current) => reduceAge(result, current), {})]));
+    return Object.fromEntries(byLocationAndAge);
+}
+
+function fetchData() {
+    return fetch(url)
+        .then(response => response.text())
+        .then(csv => parse(csv));
+}
+
+export const promise = fetchData();
+
+export function getData(json, location, age) {
+    return {
+        labels: json[location][age].map(row => row.date).reverse(),
+        datasets: [{values: json[location][age].map(row => row.incidence).reverse()}]
+    };
 }
 
 export const locations = ['Baden-Württemberg', 'Bayern', 'Berlin', 'Brandenburg', 'Bremen', 'Bundesgebiet', 'Hamburg', 'Hessen',
