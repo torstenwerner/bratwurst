4d63a64606decacd826361eb315933e3ecdefba8
Torsten Werner <torsten.werner@westernacher.com>
2021-11-15 20:41:04 +0100

    parse locations

--- bratwurst.sh
+++ bratwurst.sh
@@ -4,5 +4,14 @@ set -e
 
 CSV="https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv"
 
-curl -s $CSV | awk -F, '$2 == "Sachsen" && $4 == "60-79" {print $1, $6}' | \
-    jq -R '[inputs | split(" ") | { date: .[0], incidence: .[1] }]' > public/bratwurst.json
+curl -s $CSV > raw.csv
+
+for LOCATION in 'Baden-Württemberg' 'Bayern' 'Berlin' 'Brandenburg' 'Bremen' 'Bundesgebiet' 'Hamburg' 'Hessen' \
+    'Mecklenburg-Vorpommern' 'Niedersachsen' 'Nordrhein-Westfalen' 'Rheinland-Pfalz' 'Saarland' 'Sachsen' \
+    'Sachsen-Anhalt' 'Schleswig-Holstein' 'Thüringen'
+do
+    awk -F, '$2 == "'$LOCATION'" && $4 == "60-79" {print $1, $6}' < raw.csv | \
+        jq -R '[inputs | split(" ") | { date: .[0], incidence: .[1] }]' > public/data/$LOCATION.json
+done
+
+rm -f raw.csv
--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,25 @@
 <script>
 	import Chart from 'svelte-frappe-charts';
 	
-	let promise = fetch('/bratwurst.json')
-		.then(response => response.json())
-		.then(json => ({
-			labels: json.map(row => row.date).reverse(),
-			datasets: [{values: json.map(row => row.incidence).reverse()}]
-		}));
+	const locations = ['Baden-Württemberg', 'Bayern', 'Berlin', 'Brandenburg', 'Bremen', 'Bundesgebiet', 'Hamburg', 'Hessen',
+	    'Mecklenburg-Vorpommern', 'Niedersachsen', 'Nordrhein-Westfalen', 'Rheinland-Pfalz', 'Saarland', 'Sachsen',
+    	'Sachsen-Anhalt', 'Schleswig-Holstein', 'Thüringen'];
+	let location = 'Sachsen';
+	async function fetchData(location) {
+		return fetch(`/data/${location}.json`)
+			.then(response => response.json())
+			.then(json => ({
+				labels: json.map(row => row.date).reverse(),
+				datasets: [{values: json.map(row => row.incidence).reverse()}]
+			}));
+	}
+
+	let promise = fetchData('Sachsen')
 </script>
 
+<h1>{location}</h1>
+
 {#await promise then data}
-	<Chart title="Inzidenz" data={data} type="line" height="1000" lineOptions={{dotSize: 3}}
-		axisOptions={{xIsSeries: true, xAxisMode: 'tick'}} />	
+	<Chart title="Inzidenz" data={data} type="line" height="800" lineOptions={{dotSize: 3, regionFill: 1}}
+		axisOptions={{xIsSeries: true, xAxisMode: 'tick'}} />
 {/await}
