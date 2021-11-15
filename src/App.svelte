2d65dd85711eba394c1ee1bf8cd3de03b14a2302
Torsten Werner <torsten.werner@westernacher.com>
2021-11-15 20:15:37 +0100

    add initial chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,15 @@
 <script>
-	export let name;
-	fetch('/bratwurst.json')
+	import Chart from 'svelte-frappe-charts';
+	
+	let promise = fetch('/bratwurst.json')
 		.then(response => response.json())
-		.then(json => console.dir(json));
+		.then(json => ({
+			labels: json.map(row => row.date).reverse(),
+			datasets: [{values: json.map(row => row.incidence).reverse()}]
+		}));
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+{#await promise then data}
+	<Chart title="Inzidenz" data={data} type="line" height="1000" lineOptions={{dotSize: 3}}
+		axisOptions={{xIsSeries: true, xAxisMode: 'tick'}} />	
+{/await}
