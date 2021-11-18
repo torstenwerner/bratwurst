2a9a8e98d085ebe28b526528098284f785558be7
Torsten Werner <torsten.werner@westernacher.com>
2021-11-18 20:40:56 +0100

    simplify csv parsing

--- src/repository.js
+++ src/repository.js
@@ -1,33 +1,27 @@
 const url = 'https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv';
 
 function parse(csv) {
-    function reduceLocation(result, current) {
-        (result[current.location] = (result[current.location] || [])).push(current.value);
-        return result;
+    function rki2Frappe(dataset, csvLine) {
+        const [date, location, , age, , incidence] = csvLine.split(',');
+        if (!dataset[location]) {
+            dataset[location] = {};
+        }
+        if (!dataset[location][age]) {
+            dataset[location][age] = {
+                labels: [],
+                datasets: [{values: []}]
+            };
+        }
+        dataset[location][age].labels.push(date);
+        dataset[location][age].datasets[0].values.push(incidence);
+        return dataset;
     }
 
-    function reduceAge(result, current) {
-        (result[current.age] = (result[current.age] || [])).push(current.value);
-        return result;
-    }
-
-    function toFrappe(value) {
-        const byAge = Object.entries(value)
-            .map(([age, value]) => ([age, {
-                labels: value.map(row => row.date).reverse(),
-                datasets: [{values: value.map(row => row.incidence)}]
-            }]));
-        return Object.fromEntries(byAge);
-    }
-
-    const byLocation = csv.split('\n').slice(1)
+    return csv
+        .split('\n')
+        .slice(1)
         .reverse()
-        .map(line => line.split(','))
-        .map(([date, location, , age, , incidence]) => ({location, value: {age, value: {date, incidence}}}))
-        .reduce((result, current) => reduceLocation(result, current), {});
-    const byLocationAndAge = Object.entries(byLocation)
-        .map(([location, value]) => ([location, toFrappe(value.reduce((result, current) => reduceAge(result, current), {}))]));
-    return Object.fromEntries(byLocationAndAge);
+        .reduce((result, current) => rki2Frappe(result, current), {});
 }
 
 export const promise = fetch(url)
