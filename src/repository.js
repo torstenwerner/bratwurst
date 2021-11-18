2a1daed381a19c7c90dd86a3f750aa5a82b27222
Torsten Werner <torsten.werner@westernacher.com>
2021-11-18 21:44:08 +0100

    minor refactoring

--- src/repository.js
+++ src/repository.js
@@ -1,22 +1,22 @@
 const url = 'https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv';
 
-function parse(csv) {
-    function rki2Frappe(dataset, csvLine) {
-        const [date, location, , age, , incidence] = csvLine.split(',');
-        if (!dataset[location]) {
-            dataset[location] = {};
-        }
-        if (!dataset[location][age]) {
-            dataset[location][age] = {
-                labels: [],
-                datasets: [{values: []}]
-            };
-        }
-        dataset[location][age].labels.push(date);
-        dataset[location][age].datasets[0].values.push(incidence);
-        return dataset;
+function rki2Frappe(dataset, csvLine) {
+    const [date, location, , age, , incidence] = csvLine.split(',');
+    if (!dataset[location]) {
+        dataset[location] = {};
     }
+    if (!dataset[location][age]) {
+        dataset[location][age] = {
+            labels: [],
+            datasets: [{values: []}]
+        };
+    }
+    dataset[location][age].labels.push(date);
+    dataset[location][age].datasets[0].values.push(incidence);
+    return dataset;
+}
 
+function parse(csv) {
     return csv
         .split('\n')
         .slice(1)
