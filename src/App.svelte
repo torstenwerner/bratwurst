75ad2fb77cc27c95bfef39a298431675489e46b1
Torsten Werner <torsten.werner@westernacher.com>
2021-11-21 22:24:41 +0100

    improve styling

--- src/App.svelte
+++ src/App.svelte
@@ -8,14 +8,13 @@
 </script>
 
 <header>
-	<Select label="Region" values={locations} bind:value={location} focus/>
-	<Select label="Altersgruppe" values={ages} bind:value={age}/>
-	
 	<p>
 		Es werden die Daten des Robert-Koch-Instituts zur Hospitalisierungsinzidenz für Deutschland angezeigt.
 		Die Daten werden vom RKI täglich aktualisiert.
 		Hinweis: die Werte bis einschließlich Mai 2020 sind sehr ungenau, weil das Meldesystem zu dieser Zeit erst aufgebaut wurde.
 	</p>	
+	<Select label="Region" values={locations} bind:value={location} focus/>
+	<Select label="Altersgruppe" values={ages} bind:value={age}/>
 </header>
 
 <Chart title="Nachmeldungen" data={postChanges} type="bar" height="400" />
@@ -28,18 +27,12 @@
 <style>
 	header {
 		display: flex;
+		flex-wrap: wrap;
 		gap: 1em;
 		align-items: flex-end;
+		margin: 0 max(0px, 50vw - 55ch);
 	}
 	p {
 		margin: 0.5em 0;
-	}
-	@media (max-width: 120ch) {
-		header {
-			flex-wrap: wrap;
-		}
-		p {
-			order: -1;
-		}
 	}
 </style>
