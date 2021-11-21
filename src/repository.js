0136366e347e02ac4c34fa0c49402cafc05884aa
Torsten Werner <torsten.werner@westernacher.com>
2021-11-21 21:24:57 +0100

    calculate correction data

--- src/repository.js
+++ src/repository.js
@@ -1,22 +1,26 @@
-const url = 'https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv';
+const mainUrl = 'https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_COVID-19-Hospitalisierungen.csv';
+const correctionUrl = 'https://raw.githubusercontent.com/robert-koch-institut/COVID-19-Hospitalisierungen_in_Deutschland/master/Aktuell_Deutschland_adjustierte-COVID-19-Hospitalisierungen.csv';
 
 function rki2Frappe(dataset, csvLine) {
     const [date, location, , age, , incidence] = csvLine.split(',');
-    if (!dataset[location]) {
-        dataset[location] = {};
+    if (!!date) {
+        if (!dataset[location]) {
+            dataset[location] = {};
+        }
+        if (!dataset[location][age]) {
+            dataset[location][age] = {
+                labels: [],
+                datasets: [{values: []},{values: []}]
+            };
+        }
+        dataset[location][age].labels.push(date);
+        dataset[location][age].datasets[0].values.push(!!correctionData[date] ? incidence * correctionData[date] : null);
+        dataset[location][age].datasets[1].values.push(incidence);
     }
-    if (!dataset[location][age]) {
-        dataset[location][age] = {
-            labels: [],
-            datasets: [{values: []}]
-        };
-    }
-    dataset[location][age].labels.push(date);
-    dataset[location][age].datasets[0].values.push(incidence);
     return dataset;
 }
 
-function parse(csv) {
+function mainParse(csv) {
     return csv
         .split('\n')
         .slice(1)
@@ -24,9 +28,29 @@ function parse(csv) {
         .reduce((result, current) => rki2Frappe(result, current), {});
 }
 
-export const promise = fetch(url)
+function rki2Correction(correction, csvLine) {
+    const [date, , , , , , measuredValue, correctedValue] = csvLine.split(',')
+    if (!!date) {
+        correction[date] = correctedValue / measuredValue;
+    }
+    return correction;
+}
+
+function correctionParse(csv) {
+    return csv
+        .split('\n')
+        .slice(1)
+        .reduce((result, current) => rki2Correction(result, current), {});
+}
+
+let correctionData;
+
+export const promise = fetch(correctionUrl)
     .then(response => response.text())
-    .then(csv => parse(csv));
+    .then(csv => correctionData = correctionParse(csv))
+    .then(() => fetch(mainUrl))
+    .then(response => response.text())
+    .then(csv => mainParse(csv));
 
 export const locations = ['Baden-Württemberg', 'Bayern', 'Berlin', 'Brandenburg', 'Bremen', 'Bundesgebiet', 'Hamburg', 'Hessen',
 'Mecklenburg-Vorpommern', 'Niedersachsen', 'Nordrhein-Westfalen', 'Rheinland-Pfalz', 'Saarland', 'Sachsen',
