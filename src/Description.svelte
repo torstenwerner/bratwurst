2bd0f6d944dd8615e2a77e3361f53d1fbfcd8b60
Torsten Werner <torsten.werner@westernacher.com>
2021-11-22 21:55:53 +0100

    improve description

--- src/Description.svelte
+++ src/Description.svelte
@@ -13,13 +13,18 @@
 
 {#if opened}
     <p in:blur={{delay: 400}} out:blur>
-        Hinweis: die Werte bis einschließlich Mai 2020 sind sehr ungenau, weil das Meldesystem zu dieser Zeit erst aufgebaut wurde.
+        Die Werte bis einschließlich Mai 2020 sind sehr ungenau, weil das Meldesystem zu dieser Zeit erst aufgebaut wurde.
+    </p>
+    <p in:blur={{delay: 400}} out:blur>
+        Desweiteren gibt es einen Meldeverzug, so dass die aktuellsten Werte leider ungenau sind. Das folgende Diagram zeigt beispielhaft
+        den ursprünglichen Inzidenzwert für den 07.11.2021 und wie er jeden weiteren Tag nach oben korrigiert wurde. Nach etwa 2
+        Wochen ist der Wert knapp doppelt so hoch wie ursprünglich.
     </p>
     <div class="w-100" in:blur={{delay: 400}} out:blur>
         <Chart title="Nachmeldungen" data={postChanges} type="bar" height="400"/>
     </div>
     <p on:click={() => opened = false} class="trigger" in:blur={{delay: 400}} out:blur>
-        Informationen ausblenden...
+        Inzidenzdaten zeigen...
     </p>
 {:else}
     <p on:click={() => opened = true} class="trigger" in:blur={{delay: 400}} out:blur>
