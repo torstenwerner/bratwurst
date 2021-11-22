a501d2292fa8ad6187e1ba45ec6862c6e6c983a9
Torsten Werner <torsten.werner@westernacher.com>
2021-11-22 22:41:18 +0100

    improve transition

--- src/Description.svelte
+++ src/Description.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Chart from 'svelte-frappe-charts';
 	import {postChanges} from './repository';
-    import {blur} from "svelte/transition";
+    import {blur, slide} from "svelte/transition";
 
     export let opened = false;
 </script>
@@ -12,20 +12,22 @@
 </p>
 
 {#if opened}
-    <p in:blur={{delay: 400}} out:blur>
-        Die Werte bis einschließlich Mai 2020 sind sehr ungenau, weil das Meldesystem zu dieser Zeit erst aufgebaut wurde.
-    </p>
-    <p in:blur={{delay: 400}} out:blur>
-        Desweiteren gibt es einen Meldeverzug, so dass die aktuellsten Werte leider ungenau sind. Das folgende Diagram zeigt beispielhaft
-        den ursprünglichen Inzidenzwert für den 07.11.2021 und wie er jeden weiteren Tag nach oben korrigiert wurde. Nach etwa 2
-        Wochen ist der Wert knapp doppelt so hoch wie ursprünglich.
-    </p>
-    <div class="w-100" in:blur={{delay: 400}} out:blur>
-        <Chart title="Nachmeldungen" data={postChanges} type="bar" height="400"/>
+    <div in:slide={{delay: 400}} out:blur>
+        <p>
+            Die Werte bis einschließlich Mai 2020 sind sehr ungenau, weil das Meldesystem zu dieser Zeit erst aufgebaut wurde.
+        </p>
+        <p>
+            Desweiteren gibt es einen Meldeverzug, so dass die aktuellsten Werte leider ungenau sind. Das folgende Diagram zeigt beispielhaft
+            den ursprünglichen Inzidenzwert für den 07.11.2021 und wie er jeden weiteren Tag nach oben korrigiert wurde. Nach etwa 2
+            Wochen ist der Wert knapp doppelt so hoch wie ursprünglich.
+        </p>
+        <div>
+            <Chart title="Nachmeldungen" data={postChanges} type="bar" height="400"/>
+        </div>
+        <p on:click={() => opened = false} class="trigger" >
+            Inzidenzdaten zeigen...
+        </p>    
     </div>
-    <p on:click={() => opened = false} class="trigger" in:blur={{delay: 400}} out:blur>
-        Inzidenzdaten zeigen...
-    </p>
 {:else}
     <p on:click={() => opened = true} class="trigger" in:blur={{delay: 400}} out:blur>
         Mehr Informationen...
@@ -36,9 +38,6 @@
     p {
         margin-block: 0;
     }
-	.w-100 {
-		width: 100%;
-	}
     .trigger {
         color: var(--select-focus-border);
         cursor: pointer;
