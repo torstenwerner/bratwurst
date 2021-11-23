5a1e764645aadd0bd18865703c58729b79120821
Torsten Werner <torsten.werner@westernacher.com>
2021-11-23 22:01:18 +0100

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Chart from 'svelte-frappe-charts';
 	import {promise, locations, ages} from './repository';
-    import {blur, fade} from "svelte/transition";
+    import {blur} from "svelte/transition";
 	import Description from "./Description.svelte";
 	import Select from './Select.svelte';
 	
